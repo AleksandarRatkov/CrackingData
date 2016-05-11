8d34923a40e05847629eb74856d02e7cb1c38593
Aleksa <axela.01@gmail.com>
2016-05-11 15:43:59 +0200

    Dodat seminarski

--- backend/rest/MerchantRest/src/main/java/app/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package app.repository;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import app.model.User;
-
-
-public interface UserRepository extends MongoRepository<User, String>{
-}
